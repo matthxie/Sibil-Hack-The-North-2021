5e29d56f34b4880fd41a2c16c0e54b6f91eece36
marvisbot <madhu.mocharla@gmail.com>
2021-01-16 11:22:15 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -20,35 +20,65 @@ while cap.isOpened():
     
   if not success:
     print("Ignoring empty camera frame.")
-    # If loading a video, use 'break' instead of 'continue'.
     continue
-
-  # Flip the image horizontally for a later selfie-view display, and convert
-  # the BGR image to RGB.
   image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-  # To improve performance, optionally mark the image as not writeable to
-  # pass by reference.
   image.flags.writeable = False
   results = hands.process(image)
-
-  # Draw the hand annotations on the image.
   image.flags.writeable = True
   image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+#code starts here
   counter=0
-  if results.multi_hand_landmarks:
-    if(results.multi_handedness[0].classification[0].label == "Left"):
+  if results.multi_hand_landmarks: 
+    if(results.multi_handedness[0].classification[0].label == "Left"): #if first hand on screen is left, right click is even
         right = 0
         left = 1
-    if(results.multi_handedness[0].classification[0].label == "Right"):
+    if(results.multi_handedness[0].classification[0].label == "Right"): #if first hand on screen is right, right click is odd
         right = 1
         left = 0
+        
+    #receive output from model    
     for hand_landmarks in results.multi_hand_landmarks:
       counter = counter + 1
-      
+      # Index finger tip coordinates.
+      indexRX = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x 
+      indexRY = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y 
+      indexRZ = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].z
+
+      # Thumb tip coords
+      thumbRX = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].x 
+      thumbRY = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].y 
+      thumbRZ = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].z 
+
+      # Middle tip coords
+      middleRX = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].x
+      middleRY = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y 
+      middleRZ = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].z
+    
+      #right hand code (every alternate iteration)
       if(counter%2==right):
         mouse.moveTo(hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * width, hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * height, _pause = False)
-      mp_drawing.draw_landmarks(
-          image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+        
+        
+      #left hand code (every alternate iteration)
+      if(counter%2==left):
+        if(abs(indexRX-thumbRX) < 0.02 and abs(indexRY-thumbRY) < 0.04):
+          print(
+            f'Index Finger: '
+            f'{indexRX}, '
+            f'{indexRY}),'
+            f'{indexRZ}), '
+            '\n'
+
+            f'Thumb: '
+            f'{thumbRX}, '
+            f'{thumbRY}),'
+            f'{thumbRZ})'
+            '\n')
+          mouse.click(button = 'left')
+            
+      mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+  
   cv2.imshow('MediaPipe Hands', image)
   if cv2.waitKey(5) & 0xFF == 27:
     break
